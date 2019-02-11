6b340e02d3815aecca4c9150f8bc1f59fdedb3d7
gabrielforti <G@br13l_F0rt1>
2019-02-10 22:46:25 -0500

    css lint and template test

--- scafold2/src/features/home.js
+++ scafold2/src/features/home.js
@@ -3,6 +3,8 @@ import { RouterService } from '../services/route/index.js'
 
 const HomeCtrl = (req, next) => {
   req.load(HtmlCache.get('home'))
+  const temp = document.querySelector('template')
+  document.querySelector('.home-page').insertAdjacentHTML('beforeend', temp.innerHTML)
   next()
 }
 
